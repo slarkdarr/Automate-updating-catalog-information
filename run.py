3c16f083b39fae77807153f38529c64f1345d0a9
slarkdarr <daffaresyaly@gmail.com>
2022-03-17 01:46:38 +0700

    Add program to upload image description

---
+++ run.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+
+directory = 'supplier-data/descriptions/'                         # Directory of the feedback files
+
+keys = ['name', 'weight', 'description', 'image_name']            # List of keys
+for filename in os.listdir(directory):
+    infile = os.path.join(directory, filename)                    # Filename appended with its directory
+
+    # Read each line of the file
+    with open(infile, 'r') as f:
+        contents = f.readlines()
+
+    # Process each line of the file to a dictionary
+    fruit_dictionary = {}
+    fruit_dictionary['name'] = contents[0]
+    fruit_dictionary['weight'] = int(contents[1].strip(' lbs\n'))
+    fruit_dictionary['description'] = contents[2]
+    fruit_dictionary['image_name'] = filename.strip('.txt') + '.jpeg'
+    response = requests.post('http://34.132.71.172/fruits/', data=fruit_dictionary)     # Make POST request to the URL
+
+    # Check out the response status
+    print(response.status_code)
+    print(response.request.url)
+    print(response.request.body, end='\n\n')
