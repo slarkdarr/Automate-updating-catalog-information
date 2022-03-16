f8f2caba58266dbe60d3e06fbbc1c47c2f055db0
slarkdarr <daffaresyaly@gmail.com>
2022-03-17 02:33:36 +0700

    Tidy up functions into files

--- report_email.py
+++ report_email.py
@@ -3,9 +3,7 @@
 import os
 import datetime
 import reports
-import email.message
-import mimetypes
-import smtplib
+import emails
 
 # Set report title
 def set_title():
@@ -30,37 +28,17 @@ def set_body():
 def generate_report():
     reports.generate_report("/tmp/processed.pdf", set_title(), set_body())
 
-# Creates an email with an attachment.
-def generate_email():
-    # Basic Email formatting
-    message = email.message.EmailMessage()
-    message["From"] = "automation@example.com"
-    message["To"] = "student-01-3004faedc11a@example.com"
-    message["Subject"] = "Upload Completed - Online Fruit Store"
-    message.set_content("All fruits are uploaded to our website successfully. A detailed list is attached to this email.")
-
-    # Process the attachment and add it to the email
-    attachment_filename = os.path.basename("/tmp/processed.pdf")
-    mime_type, _ = mimetypes.guess_type("/tmp/processed.pdf")
-    mime_type, mime_subtype = mime_type.split('/', 1)
-
-    with open("/tmp/processed.pdf", 'rb') as ap:
-        message.add_attachment(ap.read(),
-                            maintype=mime_type,
-                            subtype=mime_subtype,
-                            filename=attachment_filename)
-    return message
-
-# Sends the message to the configured SMTP server.
-def send_email(message):
-    mail_server = smtplib.SMTP('localhost')
-    mail_server.send_message(message)
-    mail_server.quit()
-
 def main():
     generate_report()
-    message = generate_email()
-    send_email(message)
+    args = {
+        'sender': "automation@example.com",
+        'recipient': "student-01-3004faedc11a@example.com",
+        'subject': "Upload Completed - Online Fruit Store",
+        'body': "All fruits are uploaded to our website successfully. A detailed list is attached to this email.",
+        'attachment_path': "/tmp/processed.pdf"
+    }
+    message = emails.generate_email(**args)
+    emails.send_email(message)
 
 if __name__ == "__main__":
     main()
